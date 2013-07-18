c495822b94d6a77b0a7d694b9bda783a2a571be4
johnprock <patrickrock@dhcp-9-41-222-138.austin.ibm.com>
2013-07-18 11:05:20 -0500

    fixed broken link

--- src/helloworld/views/posts.clj
+++ src/helloworld/views/posts.clj
@@ -4,6 +4,7 @@
 
 (def p0 (post. "Hello" "<p>  My website is finally up and running in clojure. The posting system still needs some work.
 	I've been having problems with Heroku and file IO. My post files aren't able to be slurped up on the Heroku servers. I'm
-	probably going to upadate the writings section so that the main button leads to an index of all posts. Also check out http://iheanyi.com</p>" "18/7/2013" "meta web first") )
+	probably going to upadate the writings section so that the main button leads to an index of all posts. 
+	Also check out <a href=\"http://iheanyi.com\">http://iheanyi.com</a></p>" "18/7/2013" "meta web first") )
 
 (def posts [p0])
