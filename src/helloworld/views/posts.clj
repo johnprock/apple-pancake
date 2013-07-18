45a455f20abff450e139c33959eac741ab57a468
johnprock <patrickrock@dhcp-9-41-222-138.austin.ibm.com>
2013-07-18 11:00:43 -0500

    added post

--- src/helloworld/views/posts.clj
+++ src/helloworld/views/posts.clj
@@ -3,6 +3,7 @@
 (defrecord post [title content date tags])
 
 (def p0 (post. "Hello" "<p>  My website is finally up and running in clojure. The posting system still needs some work.
-	I've been having problems with Heroku and file IO. My post files aren't able to be slurped up on the Heroku servers.</p>" "18/7/2013" "meta web first") )
+	I've been having problems with Heroku and file IO. My post files aren't able to be slurped up on the Heroku servers. I'm
+	probably going to upadate the writings section so that the main button leads to an index of all posts. Also check out http://iheanyi.com</p>" "18/7/2013" "meta web first") )
 
 (def posts [p0])
